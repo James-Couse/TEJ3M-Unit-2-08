5197f6da4542862678af86fa96bc2a8f3c8a4d22
James Couse <144252195+James-Couse@users.noreply.github.com>
2023-11-27 16:23:12 +0000

    extra comment

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 """
 Created by: James Couse
 Created on: nov 24 2023
-Module uses an ultrasonic distance sensor to detect
-whether a servo should be turned or not.
+Module uses a potentiometer to control a servo
 """
 
 import time
@@ -14,9 +13,9 @@ from adafruit_motor import servo
 potentiometer = AnalogIn(board.A2)
 pwm = pwmio.PWMOut(board.A1, frequency=50)
 ServoNumber1 = servo.Servo(pwm)
-map = 66000 / 180
+map = 65535 / 180
 
 while True:
-    print ((potentiometer.value / map))
+    print(f"Pot: {potentiometer.value}, Angle: {potentiometer.value / map}")
     ServoNumber1.angle = potentiometer.value / map
-    time.sleep(0.05)
+    time.sleep(0.1)
